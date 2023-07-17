2f6f552f9a0dca24659192043dd733cfecb8cb3c
Victor Emeka Mbachu <victor.c.mbachu@gmail.com>
2023-07-17 22:26:01 +0100

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(){
         $this->call([
-//            AdminTableSeeder::class,
+            AdminTableSeeder::class,
         ]);
     }
 }
